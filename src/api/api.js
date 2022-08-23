f2a2d9f22fe5b35d30376a47aee9f01590ba36db
Pedro Reis <dropecosta@gmail.com>
2022-08-23 11:23:52 +0100

    Adding search component\

---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '0140d93960mshbc8e6cab03077eep1d1985jsne3b375a7f057',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+
+
