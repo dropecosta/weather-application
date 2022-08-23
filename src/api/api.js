9493a3de20209fdd663715b42d8b72528cc52b46
Pedro Reis <dropecosta@gmail.com>
2022-08-23 11:58:23 +0100

    Adding CurrrentWeather component and getting OpenWeather API request

--- src/api/api.js
+++ src/api/api.js
@@ -8,5 +8,5 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 
-
-
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'fa2080af14ba0504b7e12081f1eaa60a';
